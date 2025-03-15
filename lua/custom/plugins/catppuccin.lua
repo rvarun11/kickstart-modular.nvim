f1cf90ef1d3af4a6c457d2f143e8407c50f53da6
Varun Rajput <rvarun011@gmail.com>
2025-03-14 23:58:42 -0400

    update catpuccin and lockfile

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -2,10 +2,11 @@ return {
   'catppuccin/nvim',
   name = 'catppuccin',
   priority = 1000,
+  lazy = false,
   config = function()
     require('catppuccin').setup {
       flavour = 'macchiato', -- You can change this to "latte", "frappe", "macchiato", or "mocha"
-      background = { light = 'latte', dark = 'mocha' },
+      background = { light = 'latte', dark = 'macchiato' },
       transparent_background = false,
       term_colors = true,
       integrations = {
