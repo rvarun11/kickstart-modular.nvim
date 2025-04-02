bed761223facb31142f24ee2f099dd6e3013c4b2
Varun Rajput <rvarun011@gmail.com>
2025-04-02 18:59:53 -0400

    update avante.nvim

--- lua/custom/plugins/avante.lua
+++ lua/custom/plugins/avante.lua
@@ -1,21 +1,56 @@
 return {
   'yetone/avante.nvim',
   event = 'VeryLazy',
-  lazy = false,
-  version = false, -- set this if you want to always pull the latest change
+  version = false, -- Never set this value to "*"! Never!
   opts = {
-    -- add any opts here
+    provider = 'gemini',
+    auto_suggestions_provider = 'gemini',
+    cursor_applying_provider = 'gemini',
+    openai = {
+      endpoint = 'https://api.openai.com/v1',
+      model = 'gpt-4o',
+      timeout = 30000,
+      temperature = 0,
+      max_tokens = 8192,
+      -- reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+    },
+    claude = {
+      endpoint = 'https://api.anthropic.com/v1/',
+      model = 'claude-3-7-sonnet-latest',
+      timeout = 30000,
+      temperature = 0,
+      max_tokens = 8192,
+    },
+    gemini = {
+      -- endpoint = 'https://generativelanguage.googleapis.com/v1beta/',
+      model = 'gemini-2.5-pro-exp-03-25',
+      timeout = 30000,
+      temperature = 0,
+      max_tokens = 8192,
+    },
+    vendors = {
+      openrouter = {
+        __inherited_from = 'openai',
+        endpoint = 'https://openrouter.ai/api/v1',
+        api_key_name = 'OPENROUTER_API_KEY',
+        model = 'anthropic/claude-3.7-sonnet',
+      },
+    },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = 'make',
   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
   dependencies = {
+    'nvim-treesitter/nvim-treesitter',
     'stevearc/dressing.nvim',
     'nvim-lua/plenary.nvim',
     'MunifTanjim/nui.nvim',
     --- The below dependencies are optional,
+    'echasnovski/mini.pick', -- for file_selector provider mini.pick
+    'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
     'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
-    'echasnovski/mini.icons', -- or 'nvim-tree/nvim-web-devicons'
+    'ibhagwan/fzf-lua', -- for file_selector provider fzf
+    'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
     'zbirenbaum/copilot.lua', -- for providers='copilot'
     {
       -- support for image pasting
@@ -43,19 +78,4 @@ return {
       ft = { 'markdown', 'Avante' },
     },
   },
-  config = function()
-    require('avante').setup {
-      -- add any setup here
-      provider = 'openai',
-      auto_suggestions_provider = 'copilot',
-      behaviour = {
-        auto_suggestions = false, -- Experimental stage
-        auto_set_highlight_group = true,
-        auto_set_keymaps = true,
-        auto_apply_diff_after_generation = false,
-        support_paste_from_clipboard = false,
-      },
-      hints = { enabled = false },
-    }
-  end,
 }
