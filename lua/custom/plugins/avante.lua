ecbeede5993289e25164a4885b25778ad7d9f9e0
Varun Rajput <rvarun011@gmail.com>
2025-10-17 01:08:00 +0530

    feat: add copilot/claude support to avante

--- lua/custom/plugins/avante.lua
+++ lua/custom/plugins/avante.lua
@@ -2,60 +2,38 @@ return {
   'yetone/avante.nvim',
   event = 'VeryLazy',
   version = false, -- Never set this value to "*"! Never!
+  ---@module 'avante'
+  ---@type avante.Config
   opts = {
-    provider = 'gemini',
-    auto_suggestions_provider = 'gemini',
-    cursor_applying_provider = 'gemini',
+    -- add any opts here
+    -- this file can contain specific instructions for your project
+    instructions_file = 'avante.md',
+    -- for example
+    provider = 'copilot',
     providers = {
-      openai = {
-        endpoint = 'https://api.openai.com/v1',
-        model = 'gpt-4o',
-        timeout = 30000,
+      copilot = {
+        endpoint = 'https://api.githubcopilot.com',
+        model = 'claude-sonnet-4.5',
+        proxy = nil, -- [protocol://]host[:port] Use this proxy if needed
+        timeout = 30000, -- Timeout in milliseconds
+        context_window = 128000,
         extra_request_body = {
-          temperature = 0,
-          max_tokens = 8192,
-          -- reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+          temperature = 0.75,
+          max_completion_tokens = 16384,
         },
-      },
-      claude = {
-        endpoint = 'https://api.anthropic.com/v1/',
-        model = 'claude-3-7-sonnet-latest',
-        timeout = 30000,
-        extra_request_body = {
-          temperature = 0,
-          max_tokens = 8192,
-        },
-      },
-      gemini = {
-        -- endpoint = 'https://generativelanguage.googleapis.com/v1beta/',
-        model = 'gemini-2.5-pro-exp-03-25',
-        timeout = 30000,
-        extra_request_body = {
-          temperature = 0,
-          max_tokens = 8192,
-        },
-      },
-      openrouter = {
-        __inherited_from = 'openai',
-        endpoint = 'https://openrouter.ai/api/v1',
-        api_key_name = 'OPENROUTER_API_KEY',
-        model = 'anthropic/claude-3.7-sonnet',
       },
     },
   },
-  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  build = 'make',
-  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
   dependencies = {
-    'nvim-treesitter/nvim-treesitter',
-    'stevearc/dressing.nvim',
     'nvim-lua/plenary.nvim',
     'MunifTanjim/nui.nvim',
     --- The below dependencies are optional,
-    'echasnovski/mini.pick', -- for file_selector provider mini.pick
+    'nvim-mini/mini.pick', -- for file_selector provider mini.pick
     'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
     'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
     'ibhagwan/fzf-lua', -- for file_selector provider fzf
+    'stevearc/dressing.nvim', -- for input provider dressing
+    'folke/snacks.nvim', -- for input provider snacks
     'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
     'zbirenbaum/copilot.lua', -- for providers='copilot'
     {
